24aac8ef3024db848c965e0844ac157280470c7c
tziviSolo <t0504114734@gmail.com>
2025-02-09 21:59:32 +0200

    after changing the request from render

--- Node/app.js
+++ Node/app.js
@@ -1,130 +1,23 @@
 import express from "express";
+import axios from 'axios';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.type('JSON').send(List));
+const API_KEY = 'rnd_T9MOyyBvus73rpxhTd5R4ht7vjHO';
+let result;
+
+axios.get("https://api.render.com/v1/services", {
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`
+  }
+})
+  .then(response => {
+    result = response.data;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+app.get("/", (req, res) => res.type('JSON').send(result));
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
-
-const List = [
-  {
-    "cursor": "NFW8ZKzrfc9mYzVkczc4czczOW01ZTFn",
-    "service": {
-      "autoDeploy": "yes",
-      "branch": "main",
-      "createdAt": "2025-02-09T14:12:00.784524Z",
-      "dashboardUrl": "https://dashboard.render.com/web/srv-cukbfc5ds78s739m5e1g",
-      "id": "srv-cukbfc5ds78s739m5e1g",
-      "name": "node",
-      "notifyOnFail": "default",
-      "ownerId": "tea-cuh54qrv2p9s73cran1g",
-      "repo": "https://github.com/tziviSolo/FullStack",
-      "rootDir": "./Node",
-      "serviceDetails": {
-        "buildPlan": "starter",
-        "env": "docker",
-        "envSpecificDetails": {
-          "dockerCommand": "",
-          "dockerContext": ".",
-          "dockerfilePath": "./Dockerfile"
-        },
-        "healthCheckPath": "",
-        "maintenanceMode": {
-          "enabled": false,
-          "uri": ""
-        },
-        "numInstances": 1,
-        "openPorts": null,
-        "plan": "free",
-        "previews": {
-          "generation": "off"
-        },
-        "pullRequestPreviewsEnabled": "no",
-        "region": "oregon",
-        "runtime": "docker",
-        "sshAddress": "srv-cukbfc5ds78s739m5e1g@ssh.oregon.render.com",
-        "url": "https://node-lvyv.onrender.com"
-      },
-      "slug": "node-lvyv",
-      "suspended": "not_suspended",
-      "suspenders": [],
-      "type": "web_service",
-      "updatedAt": "2025-02-09T15:49:20.061033Z"
-    }
-  },
-  {
-    "cursor": "UNohNGoP5mVyaHQ2bDQ3YzczZmJvcW4w",
-    "service": {
-      "autoDeploy": "yes",
-      "branch": "main",
-      "createdAt": "2025-02-06T16:05:27.936166Z",
-      "dashboardUrl": "https://dashboard.render.com/web/srv-cuidrht6l47c73fboqn0",
-      "id": "srv-cuidrht6l47c73fboqn0",
-      "name": "Server",
-      "notifyOnFail": "default",
-      "ownerId": "tea-cuh54qrv2p9s73cran1g",
-      "repo": "https://github.com/tziviSolo/FullStack",
-      "rootDir": "./Server",
-      "serviceDetails": {
-        "buildPlan": "starter",
-        "env": "docker",
-        "envSpecificDetails": {
-          "dockerCommand": "",
-          "dockerContext": ".",
-          "dockerfilePath": "./Dockerfile"
-        },
-        "healthCheckPath": "",
-        "maintenanceMode": {
-          "enabled": false,
-          "uri": ""
-        },
-        "numInstances": 1,
-        "openPorts": null,
-        "plan": "free",
-        "previews": {
-          "generation": "off"
-        },
-        "pullRequestPreviewsEnabled": "no",
-        "region": "oregon",
-        "runtime": "docker",
-        "sshAddress": "srv-cuidrht6l47c73fboqn0@ssh.oregon.render.com",
-        "url": "https://server-rn7d.onrender.com"
-      },
-      "slug": "server-rn7d",
-      "suspended": "not_suspended",
-      "suspenders": [],
-      "type": "web_service",
-      "updatedAt": "2025-02-06T18:15:48.352787Z"
-    }
-  },
-  {
-    "cursor": "zBUEese1dYVxazl1MGptczczOWRxOTJn",
-    "service": {
-      "autoDeploy": "yes",
-      "branch": "main",
-      "createdAt": "2025-02-06T14:55:14.544928Z",
-      "dashboardUrl": "https://dashboard.render.com/static/srv-cuicqk9u0jms739dq92g",
-      "id": "srv-cuicqk9u0jms739dq92g",
-      "name": "Client",
-      "notifyOnFail": "default",
-      "ownerId": "tea-cuh54qrv2p9s73cran1g",
-      "repo": "https://github.com/tziviSolo/FullStack",
-      "rootDir": "./Client",
-      "serviceDetails": {
-        "buildCommand": "npm run build",
-        "buildPlan": "starter",
-        "previews": {
-          "generation": "off"
-        },
-        "publishPath": "build",
-        "pullRequestPreviewsEnabled": "no",
-        "url": "https://fullstack-ezbp.onrender.com"
-      },
-      "slug": "fullstack-ezbp",
-      "suspended": "not_suspended",
-      "suspenders": [],
-      "type": "static_site",
-      "updatedAt": "2025-02-08T20:05:22.329766Z"
-    }
-  }
-];
